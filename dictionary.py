885188326fe22b75d2fca3ae5ec9d6ee74d8f812
yuta-o-note <yuta.o.note@gmail.com>
2020-08-25 09:10:39 +0900

    Add dictinary sample

---
+++ dictionary.py
@@ -0,0 +1,22 @@
+# dictionaryはカーリィブラケット{}で囲む
+d = {'x': 10, 'y': 20}
+print(d)
+# {'x': 10, 'y': 20}
+print(type(d))
+# <class 'dict'>
+
+d['x'] = 100
+print(d)
+# {'x': 100, 'y': 20}
+
+d['z'] = 300
+print(d)
+# {'x': 100, 'y': 20, 'z': 300}
+
+d = dict(a = 10, b = 20)
+print(d)
+# {'a': 10, 'b': 20}
+
+d = dict([('a', 10), ('b', 20)])
+print(d)
+# {'a': 10, 'b': 20}
