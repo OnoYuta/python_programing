55964d55e780c1e50465d30f6e73e87e620ebb11
yuta-o-note <yuta.o.note@gmail.com>
2020-09-02 08:34:25 +0900

    Add range method sample

---
+++ basic_grammar/range.py
@@ -0,0 +1,14 @@
+# num_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# for i in num_list:
+#     print(i)
+
+for i in range(10):
+    print(i)
+
+# 2から開始して3飛ばしで進める
+for i in range(2, 10, 3):
+    print(i)
+
+# アンダスコアにするとループ内で使わないという説明になる
+for _ in range(10):
+    print('hello')
