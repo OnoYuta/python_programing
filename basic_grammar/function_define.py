f085147bb7974ce744ba6f08bd2f1cd002593553
yuta-o-note <yuta.o.note@gmail.com>
2020-09-02 09:13:20 +0900

    Add function default arg sample

--- basic_grammar/function_define.py
+++ basic_grammar/function_define.py
@@ -21,3 +21,35 @@ def what_is_this(color):
         return "I don't know"
 result = what_is_this('red')
 print(result)
+
+# 関数の型宣言
+def add_num(a: int, b: int) -> int:
+    return a + b
+
+r = add_num(1,2)
+print(r)
+
+# 宣言した型が一致しない場合もエラーにならない
+r = add_num('a', 'b')
+print(r)
+# ab
+
+def menu(entree, drink, dessert):
+    print(entree)
+    print(drink)
+    print(dessert)
+
+# 位置引数
+menu('beef', 'ice', 'beer')
+
+# キーワード引数
+menu(entree='beef', dessert='ice', drink='beer')
+menu('beef', dessert='ice', drink='beer')
+
+# デフォルト引数
+def default_menu(entree='beef', drink='wine', dessert='ice'):
+    print(entree)
+    print(drink)
+    print(dessert)
+
+default_menu(entree='chicken', drink='beer')
